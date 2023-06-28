258272fad4e4b220fccc8ac7475523b7db3a2650
Colin Sullender <shiruken343@gmail.com>
2023-06-28 15:32:07 -0600

    Formatting

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    loadParams()
+    loadParams();
 }, false);
 
 function getAccessToken() {
@@ -11,45 +11,47 @@ function getAccessToken() {
 function clearAccessToken() {
 	if (localStorage.getItem("access_token")) {
 		localStorage.removeItem("access_token");
-		form.elements['access_token'].value = ''
+		form.elements['access_token'].value = "";
 	}
 }
 
 function loadParams() {
 	const urlParams = new URLSearchParams(window.location.search).entries();
-	for(const param of urlParams) {
-		try{
+	for (const param of urlParams) {
+		try {
+			let value;
 			if (param[0] == "until" || param[0] == "since") {
-				value = new Date(param[1]*1000)
-				offset = new Date().getTimezoneOffset()*60000
-				value = new Date(value - offset).toISOString().slice(0,-1)
+				value = new Date(param[1] * 1000);
+				offset = new Date().getTimezoneOffset() * 60000;
+				value = new Date(value - offset).toISOString().slice(0, -1);
 			} else {
-				value = param[1]
+				value = param[1];
 			}
-			document.getElementById(param[0]).value = value
-		}
-		catch {
-			console.log("something went wrong")
+			document.getElementById(param[0]).value = value;
+		} catch(e) {
+			console.log(e);
 		}
 	}
-	getAccessToken()
+	getAccessToken();
 }
 
 async function load(url, access_token) {
 	let obj = null;
 	try {
-		obj = await (await fetch(url, { headers: { "Authorization": `Bearer ${access_token}` } })).json();
+		let headers = { headers: { "Authorization": `Bearer ${access_token}` } };
+		obj = await fetch(url, headers);
+		obj = await obj.json();
 	} catch (e) {
 		console.log(e);
 	}
-	return obj
+	return obj;
 }
 
 function fetchMore(until) {
-	getFromPS(form, until)
+	getFromPS(form, until);
 }
 
-function jsonConverter(data, renderMD, showthumbnails) {
+function generateHTML(data, renderMD, showthumbnails) {
 	let count = 0;
 	let html = "";
 	let until = 2147483647;
@@ -88,7 +90,9 @@ function jsonConverter(data, renderMD, showthumbnails) {
 				html += `
 						<div class="media-left">
 							<figure class="image is-96x96">
-								<a href="https://reddit.com${obj.permalink}" title="View post on Reddit"><img src="${obj.thumbnail}" alt="Post Thumbnail"></a>
+								<a href="https://reddit.com${obj.permalink}" title="View post on Reddit">
+									<img src="${obj.thumbnail}" alt="Post Thumbnail">
+								</a>
 							</figure>
 						</div>
 				`;
@@ -149,7 +153,10 @@ function jsonConverter(data, renderMD, showthumbnails) {
 	});
 
 	if (count > 0) {
-		html += `<button type="submit" class="button is-danger is-fullwidth my-5" id="fetch-${until}" onclick="fetchMore(${until})">Fetch More</button>`;
+		html += `
+			<button type="submit" class="button is-danger is-fullwidth my-5" 
+			id="fetch-${until}" onclick="fetchMore(${until})">Fetch More</button>
+		`;
 	}
 
 	return html;
@@ -163,79 +170,84 @@ function formatText(text, use_markdown) {
 	}
 }
 
-function getFromPS(form, until=-1){
-	if (until == -1){
+function getFromPS(form, until=-1) {
+	if (until == -1) { 	// Search
 		document.getElementById("searchButton").classList.add("is-loading");
-		document.getElementById("results").innerHTML="" 
-	} else {
+		document.getElementById("results").innerHTML = "";
+	} else { 			// Fetch More
 		document.getElementById("fetch-"+until).classList.add("is-loading");
 	}
-	path = "?"
+
+	let psURL;
+	let path = "?";
 	if (form.elements['kind'].value == "submission") {
-		psURL = "https://api.pushshift.io/reddit/submission/search?html_decode=True"
-		path += "kind=submission"
+		psURL = "https://api.pushshift.io/reddit/submission/search?html_decode=True";
+		path += "kind=submission";
 	} else {
-		psURL = "https://api.pushshift.io/reddit/comment/search?html_decode=True"
-		path += "kind=comment"
+		psURL = "https://api.pushshift.io/reddit/comment/search?html_decode=True";
+		path += "kind=comment";
 	}
 	if (form.elements['author'].value != '') {
-		psURL += "&author=" + form.elements['author'].value
-		path  += "&author=" + form.elements['author'].value
+		psURL += "&author=" + form.elements['author'].value;
+		path  += "&author=" + form.elements['author'].value;
 	}
 	if (form.elements['subreddit'].value != '') {
-		psURL += "&subreddit=" + form.elements['subreddit'].value
-		path  += "&subreddit=" + form.elements['subreddit'].value
+		psURL += "&subreddit=" + form.elements['subreddit'].value;
+		path  += "&subreddit=" + form.elements['subreddit'].value;
 	}
 	if (form.elements['min_score'].value != '') {
-		psURL += "&min_score=" + form.elements['min_score'].value
-		path  += "&min_score=" + form.elements['min_score'].value
+		psURL += "&min_score=" + form.elements['min_score'].value;
+		path  += "&min_score=" + form.elements['min_score'].value;
 	}
 	if (form.elements['max_score'].value != '') {
-		psURL += "&max_score=" + form.elements['max_score'].value
-		path  += "&max_score=" + form.elements['max_score'].value
+		psURL += "&max_score=" + form.elements['max_score'].value;
+		path  += "&max_score=" + form.elements['max_score'].value;
 	}
 	if (form.elements['since'].value != '') {
-		since = new Date(form.elements['since'].value).valueOf() / 1000
-		psURL += "&since=" + since
-		path  += "&since=" + since
+		since = new Date(form.elements['since'].value).valueOf() / 1000;
+		psURL += "&since=" + since;
+		path  += "&since=" + since;
 	}
 	if (until != -1) {
-		psURL += "&until=" + until
+		psURL += "&until=" + until;
 	} else if (form.elements['until'].value != '') {
-		until = new Date(form.elements['until'].value).valueOf() / 1000
-		psURL += "&until=" + until
-		path += "&until=" + until
+		until = new Date(form.elements['until'].value).valueOf() / 1000;
+		psURL += "&until=" + until;
+		path  += "&until=" + until;
 	}
 	if (form.elements['q'].value != '') {
-		psURL += "&q=" + encodeURIComponent(form.elements['q'].value)
-		path  += "&q=" + encodeURIComponent(form.elements['q'].value)
+		psURL += "&q=" + encodeURIComponent(form.elements['q'].value);
+		path  += "&q=" + encodeURIComponent(form.elements['q'].value);
 	}
 	if (form.elements['limit'].value == '') {
-		psURL += "&limit=100"
-		path  += "&limit=100"
+		psURL += "&limit=100";
+		path  += "&limit=100";
 	} else {
 		psURL += "&limit=" + form.elements['limit'].value;
 		path  += "&limit=" + form.elements['limit'].value;
 	}
-	history.pushState(Date.now(), "Reddit Search - Results", window.location.pathname + path)
-	access_token = form.elements['access_token'].value
+	
+	history.pushState(Date.now(), "Reddit Search - Results", window.location.pathname + path);
+	let access_token = form.elements['access_token'].value;
 	localStorage.setItem("access_token", access_token);
+
 	load(psURL, access_token).then(value => {
 		try {
-			html = jsonConverter(value.data, form.elements['renderMD'], form.elements['thumbnails'])
+			html = generateHTML(value.data, form.elements['renderMD'], form.elements['thumbnails']);
 			document.getElementById("results").innerHTML += html;
 
-			searchTerm = form.elements['q'].value
+			// Highlight Search Term(s)
+			searchTerm = form.elements['q'].value;
 			if (highlight.checked && searchTerm.length > 0) {
-				var instance = new Mark(document.querySelector("#results"));
+				let instance = new Mark(document.querySelector("#results"));
 				if (!searchTerm.startsWith('"')) {
-					searchArray = searchTerm.split(" ");
+					let searchArray = searchTerm.split(" ");
 					instance.mark(searchArray, {
 						"wildcards": "enabled",
 						"accuracy": "complementary"
 					});
 				} else {
-					term = searchTerm.replaceAll('"',"")
+					let term = searchTerm.replaceAll('"', "");
 					instance.mark(term, {
 						"accuracy": "partially",
 						"separateWordSearch": false
@@ -243,32 +255,41 @@ function getFromPS(form, until=-1){
 				}
 			}
 
-			document.getElementById("apiInfo").innerHTML = Object.keys(value.data).length + ` Results - <a href='${psURL}' target='_blank' title='View generated Pushshift API request URL' class='has-text-danger'>Generated API URL</a>`
+			document.getElementById("apiInfo").innerHTML = `
+				${value.data.length} Results - <a href='${psURL}' target='_blank' 
+				title='View generated Pushshift API request URL' class='has-text-danger'>Generated API URL</a>
+			`;
 			document.getElementById("searchButton").classList.remove("is-loading");
-			try { document.getElementById("fetch-"+until).remove() }
-			catch {}
+			try {
+				document.getElementById("fetch-" + until).remove();
+			} catch {}
 
-			var list = document.querySelectorAll(".markdown a");
+			// Inline image expansion
+			let list = document.querySelectorAll(".markdown a");
 			for (let item of list) {
-					link = item.href;
-					if (link.endsWith(".jpg") || link.endsWith(".png") || link.endsWith(".gif")) {
-							var node = document.createElement("button");
-							node.setAttribute("onclick", "directExpand('" + link + "')")
-							node.classList.add("button","is-danger","is-small")
-							var textnode = document.createTextNode("+");
-							node.appendChild(textnode);
-							item.after(node);
-					}
+				let link = item.href;
+				if (link.endsWith(".jpg") || link.endsWith(".png") || link.endsWith(".gif")) {
+					let node = document.createElement("button");
+					node.setAttribute("onclick", "directExpand('" + link + "')");
+					node.classList.add("button", "is-danger", "is-small");
+					let textnode = document.createTextNode("+");
+					node.appendChild(textnode);
+					item.after(node);
+				}
 			}
-
-		}
-		catch (error) {
-			console.log(error);
+		} catch (e) {
+			console.log(e);
 			if (value.detail == "Invalid token or expired token.") {
-				clearAccessToken()
-				document.getElementById("apiInfo").innerHTML = `Invalid or Expired Token - <a href="https://api.pushshift.io/signup" target="_blank" title="Request access token from Pushshift" class="has-text-danger">Request New Token</a>`;
+				clearAccessToken();
+				document.getElementById("apiInfo").innerHTML = `
+					Invalid or Expired Token - <a href="https://api.pushshift.io/signup" target="_blank"
+					title="Request access token from Pushshift" class="has-text-danger">Request New Token</a>
+				`;
 			} else {
-	 			document.getElementById("apiInfo").innerHTML = `Search Error: Pushshift may be down - <a href='${psURL}' target='_blank' title='View generated Pushshift API request URL' class='has-text-danger'>Generated API URL</a>`;
+	 			document.getElementById("apiInfo").innerHTML = `
+					Search Error: Pushshift may be down - <a href='${psURL}' target='_blank'
+					title='View generated Pushshift API request URL' class='has-text-danger'>Generated API URL</a>
+				`;
 			}
 			document.getElementById("searchButton").classList.remove("is-loading");
 		}
@@ -276,32 +297,32 @@ function getFromPS(form, until=-1){
 }
 
 function directExpand(link) {
-	var img = document.createElement('img');
+	let img = document.createElement('img');
 	img.src = link;
-	els = document.querySelector("a[href='" + link + "']");
+	let els = document.querySelector("a[href='" + link + "']");
 	els = els.nextElementSibling;
-	els.innerHTML = '-'
+	els.innerHTML = '-';
 	if (!els.nextElementSibling || els.nextElementSibling.id != link) {
-	  node = document.createElement("div")
-	  node.id = link;
-	  els.after(node)
+		let node = document.createElement("div");
+		node.id = link;
+		els.after(node);
 	}
-	iels = els.nextElementSibling;
+	let iels = els.nextElementSibling;
 	if (!iels.hasChildNodes()) {
-	  iels.appendChild(img);
+		iels.appendChild(img);
 	} else {
-	  if (iels.style.display == 'none') {
-		els.innerHTML = '-';
-		iels.style.display = 'block'
-	  } else {
-		els.innerHTML = '+';
-		iels.style.display = 'none';
-	  }
+		if (iels.style.display == 'none') {
+			els.innerHTML = '-';
+			iels.style.display = 'block';
+		} else {
+			els.innerHTML = '+';
+			iels.style.display = 'none';
+		}
 	}
-  }
+}
 
 const form = document.getElementById('searchForm');
 form.addEventListener('submit', (event) => {
 	event.preventDefault();
-	getFromPS(form)
+	getFromPS(form);
 });
